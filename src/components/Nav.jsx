ed28b1853e72fcf2a192fc1a448b88e8ab0be741
choi_hyk <blindlchoil@gmail.com>
2024-09-27 19:48:39 +0900

    [FE-02]: component : group card 구현

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,26 @@
-import styles from "./Nav.module.css";
+import styled from "styled-components";
 import icon from "../assets/logo/logo.png";
+
+const NavStyle = styled.div`
+    display: flex;
+    position: fixed;
+    left: 0;
+    top: 0;
+    justify-content: center;
+    align-items: center;
+    height: 64px;
+    width: 100%;
+`;
+
+const Logo = styled.img`
+    width: 64px;
+`;
 
 function Nav() {
     return (
-        <div className={styles.nav}>
-            <img src={icon} className={styles.logo} />
-        </div>
+        <NavStyle>
+            <Logo src={icon} />
+        </NavStyle>
     );
 }
 
